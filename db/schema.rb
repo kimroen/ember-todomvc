0ecd7ff182ed3808621c857868f34a78adbfe67c
Kim Røen <kim@pam.as>
2013-12-03 18:16:10 +0100

    Create the ApiKey-model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203155048) do
+ActiveRecord::Schema.define(version: 20131203170934) do
+
+  create_table "api_keys", force: true do |t|
+    t.string   "access_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "todos", force: true do |t|
     t.string   "title"
