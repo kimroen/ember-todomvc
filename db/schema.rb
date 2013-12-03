c155b7098f7d71ce117d9223a942b7584593f412
Kim Røen <kim@pam.as>
2013-12-03 18:51:30 +0100

    Making the access_token column in api_keys unique

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203170934) do
+ActiveRecord::Schema.define(version: 20131203174543) do
 
   create_table "api_keys", force: true do |t|
     t.string   "access_token"
