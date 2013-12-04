44c754ca967441a3f1e9f88624ce3e8e8ab41938
Kim Røen <kim@pam.as>
2013-12-04 14:52:00 +0100

    Move all the code to the scope of the API

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,13 @@ EmberTodomvc::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # API
-  # namespace :api, defaults: {format: 'json'} do
-  #   namespace :v1 do
-  #     resources :todos
-  #   end
-  # end
-
-  resources :users, except: [:new, :edit, :destroy]
-  post 'session' => 'session#create'
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      resources :todos
+      resources :users, except: [:new, :edit, :destroy]
+      post 'session' => 'session#create'
+    end
+  end
 
   # Send everything else to the Ember-app
   match '*path', to: 'home#index', via: [:get, :post]
