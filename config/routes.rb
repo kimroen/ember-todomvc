70a5db97fbd669efba12b33416942c7d787afdf8
Kim Røen <kim@pam.as>
2013-12-06 20:18:51 +0100

    Moving root to the top, because that's where it should be.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ EmberTodomvc::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root to: "home#index"
+
   # API
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
@@ -13,5 +15,4 @@ EmberTodomvc::Application.routes.draw do
 
   # Send everything else to the Ember-app
   match '*path', to: 'home#index', via: [:get, :post]
-  root to: "home#index"
 end
